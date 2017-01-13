c5d5a9c29133aa103c3aaa94bf8ab6e1187a0c3d
lishide <lishidezy@gmail.com>
2017-01-13 10:10:08 +0800

    get md5 方法

--- base-android-dev/src/main/java/com/base/adev/utils/CommonUtils.java
+++ base-android-dev/src/main/java/com/base/adev/utils/CommonUtils.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.MessageDigest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.regex.Pattern;
@@ -158,5 +159,34 @@ public class CommonUtils {
         styledAttributes.recycle();
 
         return toolbarHeight;
+    }
+
+    /**
+     * get md5
+     *
+     * @param s
+     * @return
+     */
+    public static String md5(String s) {
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f'};
+        try {
+            byte[] strTemp = s.getBytes();
+            MessageDigest mdTemp = MessageDigest.getInstance("MD5");
+            mdTemp.update(strTemp);
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
