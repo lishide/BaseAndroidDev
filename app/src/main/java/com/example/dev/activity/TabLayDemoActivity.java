5a36f97d87352e1b3daec955630800e92ed5dbe8
lishide <lishidezy@gmail.com>
2017-03-21 17:15:09 +0800

    提取 TabLayout 界面的初始化代码到base类中

--- app/src/main/java/com/example/dev/activity/TabLayDemoActivity.java
+++ app/src/main/java/com/example/dev/activity/TabLayDemoActivity.java
@@ -1,33 +1,16 @@
 package com.example.dev.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 
 import com.base.adev.activity.BaseTabLayActivity;
-import com.base.adev.adapter.TabAdapter;
-import com.example.dev.R;
 import com.example.dev.fragment.TabLayFragment;
 
 public class TabLayDemoActivity extends BaseTabLayActivity {
     private String title;
 
-    protected TabLayout tabLayout;
-    protected ViewPager viewPager;
-    protected TabAdapter adapterTab;
-
-    @Override
-    protected void initView() {
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-    }
-
     @Override
     protected void initLogic() {
         mToolbar.setTitle(title);
-
-        adapterTab = new TabAdapter(getSupportFragmentManager());
 
         for (int i = 0; i < 3; i++) {
             TabLayFragment fragment = new TabLayFragment();
@@ -38,12 +21,5 @@ public class TabLayDemoActivity extends BaseTabLayActivity {
     @Override
     protected void getBundleExtras(Bundle extras) {
         title = extras.getString("title");
-    }
-
-    protected void addFragment(Fragment fragment, String title) {
-        adapterTab.addFragment(fragment, title);
-        adapterTab.notifyDataSetChanged();
-        viewPager.setAdapter(adapterTab);
-        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- base-android-dev/src/main/java/com/base/adev/activity/BaseTabLayActivity.java
+++ base-android-dev/src/main/java/com/base/adev/activity/BaseTabLayActivity.java
@@ -1,10 +1,17 @@
 package com.base.adev.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 
 import com.base.adev.R;
+import com.base.adev.adapter.TabAdapter;
 
-public class BaseTabLayActivity extends BaseActivity {
+public abstract class BaseTabLayActivity extends BaseActivity {
+    protected TabLayout mTabLayout;
+    protected ViewPager mViewPager;
+    protected TabAdapter mTabAdapter;
 
     @Override
     protected void initContentView(Bundle bundle) {
@@ -13,17 +20,15 @@ public class BaseTabLayActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        mTabLayout = (TabLayout) findViewById(R.id.tl_base_tab);
+        mViewPager = (ViewPager) findViewById(R.id.vp_base_pager);
+        mTabAdapter = new TabAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mTabAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
-    @Override
-    protected void initLogic() {
-
+    protected void addFragment(Fragment fragment, String title) {
+        mTabAdapter.addFragment(fragment, title);
+        mTabAdapter.notifyDataSetChanged();
     }
-
-    @Override
-    protected void getBundleExtras(Bundle extras) {
-
-    }
-
 }
